13d054919b89207e57d06e55c59e636097b33387
prdeepak <deepak@fundthrough.com>
2016-04-21 19:02:22 -0400

    Update portfolio.R
    Add default parameters

--- R/portfolio.R
+++ R/portfolio.R
@@ -4,6 +4,6 @@ library(quantmod, warn.conflicts = FALSE, quietly = TRUE)
 library(PerformanceAnalytics, warn.conflicts = FALSE, quietly = TRUE)
 library(knitr, warn.conflicts = FALSE, quietly = TRUE)
 
-pf.portfolio.index <- function(etfs, src, weights, start.date, end.date) {
+pf.portfolio.index <- function(etfs=SPY, src="yahoo", weights=1, start.date="2000-01-01", end.date=today()) {
   # body
 }
