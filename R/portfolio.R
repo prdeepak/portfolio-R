bb57d413024761265c4497c4bb173696c9893c0d
prdeepak <deepak@fundthrough.com>
2016-04-21 18:58:03 -0400

    Create portfolio.R

---
+++ R/portfolio.R
@@ -0,0 +1,9 @@
+# Building on Adam Duncan's http://www.r-bloggers.com/monitoring-an-etf-portfolio-in-r/
+
+library(quantmod, warn.conflicts = FALSE, quietly = TRUE)
+library(PerformanceAnalytics, warn.conflicts = FALSE, quietly = TRUE)
+library(knitr, warn.conflicts = FALSE, quietly = TRUE)
+
+pf.portfolio.index <- function(etfs, src, weights, start.date, end.date) {
+  # body
+}
