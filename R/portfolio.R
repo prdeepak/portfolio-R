9a6ddc36ad54d0c4c4a62b9bf7f0670abd1e74bb
prdeepak <deepak@fundthrough.com>
2016-04-21 19:40:31 -0400

    Update portfolio.R
    get ETFs?

--- R/portfolio.R
+++ R/portfolio.R
@@ -4,6 +4,7 @@ library(quantmod, warn.conflicts = FALSE, quietly = TRUE)
 library(PerformanceAnalytics, warn.conflicts = FALSE, quietly = TRUE)
 library(knitr, warn.conflicts = FALSE, quietly = TRUE)
 
-pf.portfolio.index <- function(etfs=SPY, src="yahoo", weights=1, start.date="2000-01-01", end.date=today()) {
-  # body
+pf.portfolio.index <- function(etfs=SPY, data.source="yahoo", weights=1, start.date="2000-01-01", end.date=today()) {
+  suppressWarnings(getData(etfs, data.source))
+  # more to come
 }
