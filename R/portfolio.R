4d82599184241ab0bab106997faf026528f007a3
prdeepak <deepak1.ramachandran@gmail.com>
2016-04-21 19:43:49 -0400

    ignore packages not needed yet

--- R/portfolio.R
+++ R/portfolio.R
@@ -1,8 +1,8 @@
 # Building on Adam Duncan's http://www.r-bloggers.com/monitoring-an-etf-portfolio-in-r/
 
 library(quantmod, warn.conflicts = FALSE, quietly = TRUE)
-library(PerformanceAnalytics, warn.conflicts = FALSE, quietly = TRUE)
-library(knitr, warn.conflicts = FALSE, quietly = TRUE)
+# library(PerformanceAnalytics, warn.conflicts = FALSE, quietly = TRUE)
+# library(knitr, warn.conflicts = FALSE, quietly = TRUE)
 
 pf.portfolio.index <- function(etfs=SPY, data.source="yahoo", weights=1, start.date="2000-01-01", end.date=today()) {
   suppressWarnings(getData(etfs, data.source))
