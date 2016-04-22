afcf85ac25915cd08af702630dcad97029d9cea8
prdeepak <deepak@fundthrough.com>
2016-04-22 00:12:03 -0400

    Update portfolio.R
    pf.components grabs relevant set of ETFs within date range
    pf.index ready to weight them, create an index

--- R/portfolio.R
+++ R/portfolio.R
@@ -4,14 +4,21 @@ library(quantmod, warn.conflicts = FALSE, quietly = TRUE)
 # library(PerformanceAnalytics, warn.conflicts = FALSE, quietly = TRUE)
 # library(knitr, warn.conflicts = FALSE, quietly = TRUE)
 
-pf.index <- function(etf.Symbols ="SPY", data.source="yahoo", weights=1, start.date="2000-01-01", end.date=Sys.Date()) {
+pf.components <- function(etf.Symbols ="SPY", data.source="yahoo", start.date="2000-01-01", end.date=Sys.Date()) {
   getSymbols(Symbols = etf.Symbols, src = data.source, warnings = FALSE)
-
-  date.range <- paste(start.date, end.date, sep="::")
   etfs <- lapply(etf.Symbols, get)
+  names(etfs) <- etf.Symbols
+  
+  date.range <- paste(start.date, end.date, sep="::")
+  etfs <- lapply(etfs, function(x){x <- x[date.range]})
+  
+  return(etfs)
+}  # pf.components
+
+pf.index <- function(etf.Symbols ="SPY", data.source="yahoo", weights=1, start.date="2000-01-01", end.date=Sys.Date()) {
+  etfs <- pf.components(etf.Symbols = etf.Symbols, data.source = data.source, start.date = start.date, end.date = end.date)
   
   weights <- weights / sum(weights)  # force weights to add to 1
   
-  return(etfs)
   # more to come
-}
+} # pf.index
